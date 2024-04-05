3ea6b24a09d73f4f2c2af8469c181f0c2e113abd
khoon68 <rhdqndyd0608@gmail.com>
2024-04-05 13:36:42 +0900

    Create MemberService.java

---
+++ aa/src/main/java/springreact/aa/service/MemberService.java
@@ -0,0 +1,34 @@
+package springreact.aa.service;
+
+import org.springframework.stereotype.Service;
+import springreact.aa.domain.Member;
+import springreact.aa.repository.MemberRepository;
+import springreact.aa.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicate(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    private void validateDuplicate(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("중복된 이름입니다.");
+        });
+    }
+}
